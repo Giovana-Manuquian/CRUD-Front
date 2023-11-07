46e7217ba8fe9e793fd4e78ef2cf2fa6c0f6d7cd
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2023-11-07 10:03:21 -0300

    Tela Cadastro

---
+++ js/cadastro.js
@@ -0,0 +1,6 @@
+//Adicionar um evento ao botão "Adicionar Card"
+const retornarButton = document.getElementById('returnButton');
+retornarButton.addEventListener('click', () => {
+    const index = "../../index.html"
+    window.location.href = index    
+});
