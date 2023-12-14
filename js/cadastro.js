e9a22ac83045ed44d7ef7ffa5b33916f59059442
Giovana-Manuquian <gmanuquian@gmail.com>
2023-12-13 22:29:12 -0300

    front atualizado

--- js/cadastro.js
+++ js/cadastro.js
@@ -4,13 +4,13 @@ retornarButton.addEventListener('click', () => {
     window.location.href = index
 });
 
-
 async function cadastrarContato() {
     const nome = document.getElementById('nome').value;
     const telefone = document.getElementById('telefone').value;
     const email = document.getElementById('email').value;
-    const favorito = document.getElementById('favorito').value;
-    const foto = document.getElementById('foto').value; // Assume que 'foto' é uma string representando a imagem
+    const favoritoElement = document.getElementById('favorito');
+    const favorito = favoritoElement.checked ? 1 : 0;
+    const foto = document.getElementById('foto').value;
 
     const contatoData = {
         nome,
@@ -19,8 +19,6 @@ async function cadastrarContato() {
         email,
         foto,
     };
-
-    console.log(contatoData.foto)
 
     try {
         const response = await fetch('http://127.0.0.1:5600/cadastrarContato', {
@@ -35,17 +33,17 @@ async function cadastrarContato() {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
-        const responseData = await response.json();
+        // Adiciona um console.log para visualizar a resposta do servidor
+        console.log('Resposta do servidor:', await response.text());
+        // Adiciona uma mensagem de sucesso
+        alert("Contato Cadastrado com Sucesso!!");
 
-        console.log(responseData);
-        alert("Contato Cadastrado com Sucesso !!");
-
-        const index = "../../index.html";
-        window.location.href = index;
+        // Adiciona um pequeno atraso (500 milissegundos) antes de recarregar a página
+        setTimeout(() => {
+            window.location.reload(true);
+        }, 500);
     } catch (error) {
         console.error('Erro:', error);
         alert("Erro ao Cadastrar Contato!!");
     }
 }
-
-
